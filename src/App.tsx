0fd70a3a1fa9aecaf3760c3b3a18a005f8f0745d
Duncan <dcrmccombied@yahoo.com>
2026-03-12 11:47:40 -0700

    Faded logo added

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,7 @@ function App() {
         <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4 lg:px-8">
           <div className="flex items-center gap-3">
   <img
-    src="/elevionique website banner fade.png"
+    src="/elevionique website banner fade 3-12-26.png"
     alt="Elevionique"
     className="h-24 w-auto"
   />
